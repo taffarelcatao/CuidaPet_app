1d6563207de6ad2d21726417be387e8d2e803abc
taffarelcatao <taffarel-182@hotmail.com>
2023-08-11 16:36:22 -0400

    adicionando icones e formandando o textForm

--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -1,7 +1,11 @@
+import 'package:cuidapet_mobile/app/core/ui/icons/cuidapet_icons.dart';
+import 'package:cuidapet_mobile/app/core/ui/widgets/cuidapet_textform_field.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  final testeEC = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +13,38 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Login Page'),
       ),
-      body: Container(),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              CuidapetTextFormField(
+                label: 'Login',
+                obscureText: false,
+                controller: testeEC,
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "Valor Obrigatorio";
+                  }
+                  return null;
+                },
+              ),
+              Text(testeEC.text),
+              ElevatedButton(
+                  onPressed: () {
+                    formKey.currentState?.validate();
+                    print(testeEC.text);
+                  },
+                  child: Text('Salvar')),
+              Icon(
+                CuidapetIcons.facebook,
+              ),
+              Icon(CuidapetIcons.google),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
